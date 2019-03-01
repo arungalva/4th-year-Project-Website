4fac36008b9e2899d9dc9685aee59381bf4afbab
seanrt <seanrtohidi@gmail.com>
2019-02-28 21:05:39 -0500

    The User email field is now mandatory. Regex is used to validate the email.

--- src/main/java/com/eggplant/emoji/entities/User.java
+++ src/main/java/com/eggplant/emoji/entities/User.java
@@ -29,7 +29,6 @@ public class User {
     private Role role;
     private String first_name;
     private String last_name;
-    // The User email should belong to the carleton.ca domain
     private String email;
     private Program program;
 
@@ -70,6 +69,8 @@ public class User {
 
     public void setLastName(String last_name) { this.last_name = last_name; }
 
+    @NotNull
+    @Pattern(regexp = "\\S+@(cmail\\.)?carleton.ca", message = "Email must be a valid email belonging to the (cmail.)carleton.ca domain.")
     public String getEmail() { return this.email; }
 
     public void setEmail(String email) { this.email = email; }
