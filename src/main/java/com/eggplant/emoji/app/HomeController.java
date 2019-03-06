5d15a44ddc2d77c3ea2050fd1d5dea8340488bd8
blessingomotayo <blessing.omotayo@carleton.ca>
2019-03-06 02:50:55 -0500

    Added tests for the new pages created

--- src/main/java/com/eggplant/emoji/app/HomeController.java
+++ src/main/java/com/eggplant/emoji/app/HomeController.java
@@ -1,15 +1,31 @@
 package com.eggplant.emoji.app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public @ResponseBody String greeting() {
-        return "Hello World";
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/deadlines")
+    public String deadlines() {
+        return "deadlines";
+    }
+
+    @GetMapping("/notices")
+    public String notices() {
+        return "notices";
+    }
+
+    @GetMapping("/projects")
+    public String projects() {
+        return "projects";
     }
 
 }
