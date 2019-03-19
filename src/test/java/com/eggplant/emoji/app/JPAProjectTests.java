c94749da89d5abd43cd8f804c97a53d2e476e326
blessingomotayo <blessing.omotayo@carleton.ca>
2019-03-18 23:27:17 -0400

    updated test case

--- src/test/java/com/eggplant/emoji/app/JPAProjectTests.java
+++ src/test/java/com/eggplant/emoji/app/JPAProjectTests.java
@@ -166,4 +166,67 @@ public class JPAProjectTests {
         assertEquals("archivedProjects", modelAndView.getViewName());
 
     }
+
+    /**
+     * Sends a POST request to add a project to the DB and test if that project was removed to the database
+     * @throws Exception
+     */
+    @Test
+    public void archiveProject() throws Exception {
+        MvcResult result = this.mockMvc.perform(post("/project/add")
+                .param("projectName","Test Project")
+                .param("description","Test Project Description")
+                .param("minNumberOfStudents","2")
+                .param("maxNumberOfStudents","5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Test Project")))
+                .andReturn();
+        ModelAndView modelAndView = result.getModelAndView();
+        assertNotNull(modelAndView);
+        assertNotNull(modelAndView.getViewName());
+        assertEquals("professor", modelAndView.getViewName());
+        @SuppressWarnings("unchecked")
+        List<Project> projects = (List<Project>) modelAndView.getModel().get("projects");
+        Project projectExp = null;
+        for(Project project : projects){
+            if(project.getProjectName().equals("Test Project")){
+                projectExp = project;
+            }
+        }
+        assertNotNull(projectExp);
+        assertEquals("Test Project", projectExp.getProjectName());
+        assertEquals("Test Project Description", projectExp.getDescription());
+        assertEquals(2, projectExp.getMinNumberOfStudents());
+        assertEquals(5, projectExp.getMaxNumberOfStudents());
+        assertEquals(null, projectExp.getArchivedDate());
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-test");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        Query q = em.createQuery("SELECT p FROM Project p WHERE p.projectName = 'Test Project'");
+        @SuppressWarnings("unchecked")
+        List<Project> results = q.getResultList();
+        assertEquals(1, results.size());
+
+        Project p = results.get(0);
+        p.archiveProject();
+        assertNotNull(p.getArchivedDate());
+
+        tx.begin();
+        em.persist(p);
+        tx.commit();
+
+        Query query = em.createQuery("SELECT p FROM Project p WHERE p.archivedDate != null");
+        @SuppressWarnings("unchecked")
+        List<Project> archivedResults = query.getResultList();
+        assertEquals(1, archivedResults.size());
+        assertNotNull(archivedResults.get(0).getArchivedDate());
+
+        tx.begin();
+        em.remove(results.get(0));
+        tx.commit();
+
+        em.close();
+        emf.close();
+    }
 }
