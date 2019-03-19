85d21383643dee825284eb48a15549465416c913
Arun Galva <arungalva@hotmail.com>
2019-03-19 14:52:13 -0400

    testing some stuff

--- src/main/java/com/eggplant/emoji/AppApplication.java
+++ src/main/java/com/eggplant/emoji/AppApplication.java
@@ -3,6 +3,7 @@ package com.eggplant.emoji;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// hello
 @SpringBootApplication
 public class AppApplication {
 
