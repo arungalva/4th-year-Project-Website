0b75af97cb9393cb784f873eae80774aa0d51a4a
seanrt <seanrtohidi@gmail.com>
2019-03-19 21:12:33 -0400

    Added the updateProject() method to the ProjectService. Updated the projectrepository to add the findAllByOrderByIdAsc() method.

--- src/main/java/com/eggplant/emoji/repository/ProjectRepository.java
+++ src/main/java/com/eggplant/emoji/repository/ProjectRepository.java
@@ -1,16 +1,15 @@
 package com.eggplant.emoji.repository;
 
 import com.eggplant.emoji.entities.Project;
-import com.eggplant.emoji.entities.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Date;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
     Project findByProjectName(String projectName);
+    List<Project> findAllByOrderByIdAsc();
 
     @Modifying
     @Transactional
