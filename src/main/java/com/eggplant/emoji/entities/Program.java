c7f3e070c024be4f3435d24b99ffc72ee5328572
ShasRanasinghe <shasthraranasinghe@hotmail.com>
2019-03-29 10:32:19 -0400

    Issue-74: Changed program into an entity

--- src/main/java/com/eggplant/emoji/entities/Program.java
+++ src/main/java/com/eggplant/emoji/entities/Program.java
@@ -1,13 +1,25 @@
 package com.eggplant.emoji.entities;
 
-public enum Program {
-    BIOMEDICAL_ELECTRICAL("Biomedical Electrical", "BE"),
-    COMMUNICATION_ENGINEERING("Communication Engineering", "CE"),
-    COMPUTER_SYSTEMS_ENGINEERING("Computer Systems Engineering", "CSE"),
-    SOFTWARE_ENGINEERING("Software Engineering", "SE"),
-    ELECTRICAL_ENGINEERING("Electrical Engineering", "EE");
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "program")
+public class Program {
+//    BIOMEDICAL_ELECTRICAL("Biomedical Electrical", "BE"),
+//    COMMUNICATION_ENGINEERING("Communication Engineering", "CE"),
+//    COMPUTER_SYSTEMS_ENGINEERING("Computer Systems Engineering", "CSE"),
+//    SOFTWARE_ENGINEERING("Software Engineering", "SE"),
+//    ELECTRICAL_ENGINEERING("Electrical Engineering", "EE");
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
     private String programName;
+
+    @NotNull
     private String programCode;
 
     Program(String programName, String programCode) {
@@ -15,7 +27,15 @@ public enum Program {
         this.programCode = programCode;
     }
 
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
     public String getProgramName() { return programName; }
+    public void setProgramName(String programName) { this.programName = programName; }
 
     public String getProgramCode() { return programCode; }
+    public void setProgramCode(String programCode) { this.programCode = programCode; }
+
+
+
 }
