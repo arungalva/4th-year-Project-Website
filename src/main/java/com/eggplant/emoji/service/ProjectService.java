339d1639682755986447232ca2a98b88140f745f
blessingomotayo <blessing.omotayo@carleton.ca>
2019-04-02 12:37:52 -0400

    Fixed merge conflicts

--- src/main/java/com/eggplant/emoji/service/ProjectService.java
+++ src/main/java/com/eggplant/emoji/service/ProjectService.java
@@ -5,7 +5,6 @@ import com.eggplant.emoji.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
