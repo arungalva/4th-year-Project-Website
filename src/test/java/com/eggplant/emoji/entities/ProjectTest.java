3e4f30849e240588d847481ffd45817849af7cfc
blessingomotayo <blessing.omotayo@carleton.ca>
2019-04-02 23:56:36 -0400

    added more tests for project entity

--- src/test/java/com/eggplant/emoji/entities/ProjectTest.java
+++ src/test/java/com/eggplant/emoji/entities/ProjectTest.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.EnumSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -37,5 +36,71 @@ public class ProjectTest {
         assertTrue(p2.getProgramRestrictions().contains(Program.SOFTWARE_ENGINEERING));
         assertTrue(p2.getProgramRestrictions().contains(Program.BIOMEDICAL_ELECTRICAL));
         repo.delete(this.p);
+    }
+
+    @Test
+    public void testGetAndSetID() {
+        Long id = 12345L;
+
+        this.p = new Project();
+        this.p.setId(id);
+
+        assertEquals(id, p.getId());
+    }
+
+    @Test
+    public void getAndSetDescription() {
+        String testDescription = "This is a test description";
+        this.p = new Project();
+        p.setDescription(testDescription);
+
+        assertEquals(testDescription, p.getDescription());
+    }
+
+    @Test
+    public void getAndSetProjectName() {
+        this.p = new Project();
+        p.setProjectName(projectName);
+
+        assertEquals(projectName, p.getProjectName());
+    }
+
+    @Test
+    public void getAndSetMinNumberOfStudents() {
+        int testMinNumberOfStudents = 2;
+        this.p = new Project();
+        p.setMinNumberOfStudents(testMinNumberOfStudents);
+
+        assertEquals(testMinNumberOfStudents, p.getMinNumberOfStudents());
+    }
+
+    @Test
+    public void getAndSetMaxNumberOfStudents() {
+        int testMaxNumberOfStudents = 5;
+        this.p = new Project();
+        p.setMaxNumberOfStudents(testMaxNumberOfStudents);
+
+        assertEquals(testMaxNumberOfStudents, p.getMaxNumberOfStudents());
+    }
+
+    @Test
+    public void getAndSetArchiveDate() {
+        Date date = new Date();
+        this.p = new Project();
+        p.setArchivedDate(date);
+
+        assertEquals(date, p.getArchivedDate());
+    }
+
+    @Test
+    public void addAndRemoveStudents() {
+        User student = new User();
+        User student2 = new User();
+
+        this.p = new Project();
+        p.addStudent(student);
+        p.addStudent(student2);
+
+        assertEquals(true, p.removeStudent(student));
     }
 }
